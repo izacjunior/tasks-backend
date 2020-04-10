483a02c09a100bb7c1bd811c451b80994ff2f31d
Izac <izacjunior@gmail.com>
2020-04-10 10:51:39 -0300

    testes

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,45 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+	@Test
+	public void deveRetornarTrueParaDatasFuturas() {
+		
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		System.out.println(DateUtils.isEqualOrFutureDate(date));
+		DateUtils.isEqualOrFutureDate(date);
+	
+		assertTrue(DateUtils.isEqualOrFutureDate(date));
+	
+	}
+	
+	@Test
+	public void deveRetornarFalseParaDatasFalse() {
+		
+		LocalDate date = LocalDate.of(2010, 01, 01);
+		System.out.println(DateUtils.isEqualOrFutureDate(date));
+		DateUtils.isEqualOrFutureDate(date);
+	
+		assertFalse(DateUtils.isEqualOrFutureDate(date));
+	
+	}
+	
+	@Test
+	public void deveRetornarTrueParaDataAtual() {
+		
+		LocalDate date = LocalDate.now();
+		System.out.println(DateUtils.isEqualOrFutureDate(date));
+		DateUtils.isEqualOrFutureDate(date);
+	
+		assertTrue(DateUtils.isEqualOrFutureDate(date));
+	
+	}
+	
+}
